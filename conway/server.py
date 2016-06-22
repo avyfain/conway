c668f6186b35d65d79c36e9c139a9eac31f159aa
avyfain <avyfain@gmail.com>
2016-06-22 01:03:47 -0700

    iterate through all tweets, not just one

--- conway/server.py
+++ conway/server.py
@@ -13,30 +13,26 @@ import redis
 r = redis.from_url(os.environ.get("REDIS_URL"))
 
 def grab_tweets():
-    tweet = None
     max_id = r.get('max_id')
-    while not tweet:
-        tweets = twitter_client.search('@tweetgameoflife', max_id)
-        try:
-            tweet = next(tweet for tweet in tweets if '://' not in tweet.text and \
-                                                      not tweet.text.startswith('RT'))
-            r.set('max_id', tweets[0].id_str)
-            return tweet
-        except StopIteration:
-            return None
-
-def reply_with_conway():
-    original_tweet = grab_tweets()
-    if original_tweet:
-        pattern = create_pattern_from_tweet(original_tweet)
-        gif_name = gif_from_pattern(pattern)
-        user_name = original_tweet.user.screen_name
-        message = "Hi @{}, look at the complexity of your tweet!".format(user_name)
-        twitter_client.update_with_media(filename=gif_name, 
-                                  status=message, 
-                                  in_reply_to_status_id=original_tweet.id_str)
+    tweets = twitter_client.search('@tweetgameoflife', since_id=max_id)
+    if tweets:
+        r.set('max_id', str(tweets[0].id + 1))
     else:
         print("No one is tweeting, try again later :(")
+    
+    for tweet in tweets:
+        if '://' not in tweet.text and not tweet.text.startswith('RT'):
+            yield tweet
+
+
+def reply_with_conway(original_tweet):
+    pattern = create_pattern_from_tweet(original_tweet)
+    gif_name = gif_from_pattern(pattern)
+    user_name = original_tweet.user.screen_name
+    message = "Hi @{}, look at the complexity of your tweet!".format(user_name)
+    twitter_client.update_with_media(filename=gif_name, 
+                              status=message, 
+                              in_reply_to_status_id=original_tweet.id_str)
 
 def get_word_bin(string):
     return ''.join(format(ord(x), 'b') for x in string)
@@ -55,4 +51,6 @@ def gif_from_pattern(pattern):
     return name
 
 if __name__ == '__main__':
-    reply_with_conway()
+    tweets = grab_tweets()
+    for tweet in tweets:
+        reply_with_conway(tweet)
